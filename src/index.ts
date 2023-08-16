6f6d3b1b029cf2069933482d48dcaa4f13729a42
Eliza Hynek <eliza.hynek@gmail.com>
2023-08-16 17:57:07 +0200

    add UsersData class

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ const inquirer = require('inquirer');
 const consola = require('consola');
 
 enum Action {
-	List = "list",
-	Add = "add",
-	Remove = "remove",
-	Quit = "quit"
+	List = 'list',
+	Add = 'add',
+	Remove = 'remove',
+	Quit = 'quit'
 }
 
 enum MessageVariant {
@@ -65,3 +65,52 @@ class Message {
     }
   }
 }
+
+interface User {
+	name: string,
+	age: number
+}
+
+class UsersData {
+	private data: User[] = [];
+
+	showAll(): void {
+    Message.showColorized(MessageVariant.Info, 'Users data');
+    if (this.data.length > 0) {
+      console.table(this.data);
+    } else {
+      Message.showColorized(MessageVariant.Error, 'No data...');
+    }
+  }
+
+	public add(user: User): void {
+		if (typeof user.age === 'number' && typeof user.name === 'string' && user.age > 0 && user.name.length > 0) {
+			this.data.push(user);
+			Message.showColorized(MessageVariant.Success, 'User has been successfully added!');
+		} else {
+			Message.showColorized(MessageVariant.Error, 'Wrong data!')
+		}
+	}
+
+	public remove(name: string) {
+		const index = this.data.findIndex(user => user.name === name);
+		if (index !== -1) {
+			this.data.splice(index, 1);
+			Message.showColorized(MessageVariant.Success, 'User deleted!');
+		} else {
+			Message.showColorized(MessageVariant.Error, 'User not found...')
+		}
+	}
+
+}
+
+const users = new UsersData();
+users.showAll();
+users.add({ name: "Jan", age: 20 });
+users.add({ name: "Adam", age: 30 });
+users.add({ name: "Kasia", age: 23 });
+users.add({ name: "Basia", age: -6 });
+users.showAll();
+users.remove("Maurycy");
+users.remove("Adam");
+users.showAll();
